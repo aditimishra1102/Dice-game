2582a44045d1d49cf2451d6a77d58a16881ddac0
aditimishra1102 <106749713+aditimishra1102@users.noreply.github.com>
2023-07-11 04:46:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randonDiceImage1 ="dice"+ randomNumber1 + ".png";  //dice1.png - dice6.png
+
+var image1= document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randonDiceImage1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randonDiceImage2 ="dice"+ randomNumber2 + ".png";  //dice1.png - dice6.png
+
+var image2= document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randonDiceImage2);
+
+//IF PLAYER 1 WINS
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw!";
+}
